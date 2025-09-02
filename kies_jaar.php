f630d75f45c1a395419ce38f15cf9c91716abd18
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-02 11:19:36 +0200

    e

--- kies_jaar.php
+++ kies_jaar.php
@@ -29,8 +29,7 @@ if (isset($_SESSION['jaar']) and $_SESSION['jaar'] != '') $evaluatie_tabel = 'ev
 if (empty($_SESSION['jaar']) or $_SESSION['jaar'] == 2025) $_SESSION['cursusoffset'] = 59;
 if (empty($_POST['cursusnr']) AND empty($_SESSION['cursusnr'])) $_SESSION['cursusnr'] = 0;
 if (isset($_POST['cursusnr'])) $_SESSION['cursusnr'] = $_POST['cursusnr'];
-$_SESSION['cursus'] = $_SESSION['cursusnr'] + $_SESSION['cursusoffset'];
-if ($_SESSION['cursus'] > 0) $_SESSION['zoek_cursus'] = "WHERE cursus = {$_SESSION['cursus']}";
+if ($_SESSION['cursusnr'] > 0) $_SESSION['zoek_cursus'] = "WHERE cursus = {$_SESSION['cursus']}";
 else $_SESSION['zoek_cursus'] = '';
 
 d($_GET, $_POST, $evaluatie_tabel, $_SESSION);
