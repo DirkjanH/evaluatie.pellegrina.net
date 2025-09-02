407dfa461607062e578653ed43ddca3dc01186ec
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-02 09:59:47 +0200

    e

--- rapport_publiciteit.php
+++ rapport_publiciteit.php
@@ -1,4 +1,8 @@
 <?php
+// stel php in dat deze fouten weergeeft
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 $naam = "publicity";
 $punten = "publiciteit";
 require_once('genereer_rapport_1a.php');?>
