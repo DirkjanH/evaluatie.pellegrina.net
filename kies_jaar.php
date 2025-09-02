89eeee649d3d42b90519e42a3bcbb9b39cff8138
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-02 11:23:42 +0200

    e

--- kies_jaar.php
+++ kies_jaar.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 require_once('connections/PDO_connect.php');
 require_once $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
 
-Kint::$enabled_mode = true;
+Kint::$enabled_mode = false;
 
 // zet de tijdzone:
 date_default_timezone_set('Europe/Berlin');
@@ -25,8 +25,6 @@ elseif ($_GET['jaar'] >= 2006 and $_GET['jaar'] <= $jaar) $_SESSION['jaar'] = $_
 else echo 'Dit is geen geldig jaar!<br>';
 
 if (isset($_SESSION['jaar']) and $_SESSION['jaar'] != '') $evaluatie_tabel = 'evaluatie_' . $_SESSION['jaar'];
-
-if (empty($_SESSION['jaar']) or $_SESSION['jaar'] == 2025) $_SESSION['cursusoffset'] = 59;
 if (empty($_POST['cursusnr']) AND empty($_SESSION['cursusnr'])) $_SESSION['cursusnr'] = 0;
 if (isset($_POST['cursusnr'])) $_SESSION['cursusnr'] = $_POST['cursusnr'];
 if ($_SESSION['cursusnr'] > 0) $_SESSION['zoek_cursus'] = "WHERE cursus = {$_SESSION['cursusnr']}";
