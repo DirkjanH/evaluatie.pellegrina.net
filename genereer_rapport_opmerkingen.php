f5598860884cb9c7d38379e419637691aa9b620c
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-09-20 15:33:01 +0200

    Add left alignment for second column in remarks report table and clean up HTML structure

--- genereer_rapport_opmerkingen.php
+++ genereer_rapport_opmerkingen.php
@@ -12,30 +12,33 @@ $totalRows_report = count($report);
 ?>
 <!DOCTYPE HTML>
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="utf-8">
-<html>
-<head>
-<title>Report <?php echo $naam; ?></title>
-<link href="css/evaluatie.css" rel="stylesheet" type="text/css">
-</head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="utf-8">
+   <html>
+
+   <head>
+      <title>Report <?php echo $naam; ?></title>
+      <link href="css/evaluatie.css" rel="stylesheet" type="text/css">
+   </head>
 
 <body>
-<?php echo ("<h2>Report {$naam}</h2>") ?>
-<table id="opmerkingen">
-   <tr>
-      <th width="15%">name:</th>
-      <th>remarks:</th>
-   </tr>
-   <?php
-foreach ($report as $rep) {
-  if ($rep[$opmerkingen] != NULL)
-echo "<tr><td>{$rep['naam']}</td>
+   <?php echo ("<h2>Report {$naam}</h2>") ?>
+   <table id="opmerkingen" class="remarks-report">
+      <tr>
+         <th width="15%">name:</th>
+         <th>remarks:</th>
+      </tr>
+      <?php
+      foreach ($report as $rep) {
+         if ($rep[$opmerkingen] != NULL)
+            echo "<tr><td>{$rep['naam']}</td>
          <td>{$rep[$opmerkingen]}</td>
       </tr>";
-  }
-?>
-</table>
+      }
+      ?>
+   </table>
 </body>
+
 </html>
